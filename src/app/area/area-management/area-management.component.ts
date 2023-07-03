a387e44cac8aa624e1bed5d5e0011b8b0f83ab3c
Krishna Bhandarkar <krishna.b@copperdigital.com>
2023-07-03 11:08:39 +0530

    changed hard coded data of table  to for loop in area  management component

--- src/app/area/area-management/area-management.component.ts
+++ src/app/area/area-management/area-management.component.ts
@@ -10,5 +10,49 @@ export class AreaManagementComponent {
 
   constructor(public general: LoginServiceService) { }
 
+  change_input_check(event: any) {
+    if (event.target.checked == true) {
+      this.check = true;
+    }
+    else {
+      this.check = false;
+    }
+  }
+
+
+  public check = false;
+
+
+  public area_management_data = [
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+    {
+      id: 1,
+      area: 'Lane 7',
+    },
+  
+
+   
+
+  ]
+
 
 }
